a4bb768fb08800532e5d5a3e8fec6c5d908198bd
stacy <sl767@njit.edu>
2020-03-10 14:22:32 -0400

    task 1: created RNG pick num from list task 2: created RNG pick num from list with seed

--- RandomNumGenerator/numbersFromList.py
+++ RandomNumGenerator/numbersFromList.py
@@ -1,13 +1,14 @@
 import random
 
+
 class PickNoSeed():
     @staticmethod
     def pickNumbers(aList, rangeNum):
-        newList =[]
+        newList = []
         listSize = len(aList)
 
         for each in range(rangeNum):
-            position = random.randint(0, listSize-1)
+            position = random.randint(0, listSize - 1)
             newList.append(aList[position])
 
         return newList
