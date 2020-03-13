5cf1f5e2c1ea591a8cb48a0659dea96a6109a6c6
brandonRodriguez24 <br292@njit.edu>
2020-03-13 15:33:24 -0400

    created a file to test statistics

---
+++ Tests/test_Statistics.py
@@ -0,0 +1,16 @@
+import unittest
+from numpy.random import seed
+from numpy.random import randint
+from Statistics.Statistics import Statistics
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        seed(5)
+        self.testData = randint(0, 10, 20)
+        self.statistics = Statistics()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+if __name__ == '__main__':
+    unittest.main()
